2ee8875253cbbbd155ef363df4f04de704adc8c0
Jonah Dahlquist <jonah@nucleussystems.com>
2015-11-22 09:30:31 -0800

    Added region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',
     aws_access_key_id:     ENV['AWS_S3_KEY_ID'],
     aws_secret_access_key: ENV['AWS_S3_ACCESS_KEY'],
+    region:                'us-west-2'
   }
   config.fog_directory  = ENV['AWS_S3_BUCKET']                             # required
   # config.s3_access_policy = :public_read
