c88ac6446faaf98b303b9a96ea90e7960c437cd5
Jonah Dahlquist <jonah@nucleussystems.com>
2015-11-21 16:16:18 -0800

    Added timestamp field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,11 @@
 $(function () {
     var loadingIndicator = $('#loading-location');
     var errorIndicator = $('#error-location');
+    var bomb_form = $('#new_bomb');
     var latitude = $('#bomb_latitude');
     var longitude = $('#bomb_longitude');
     var accuracy = $('#bomb_accuracy');
+    var taken_at = $('#bomb_taken_at');
     $('#photobombed').click(function (e) {
         e.preventDefault();
         loadingIndicator.show();
@@ -31,6 +33,8 @@ $(function () {
                     latitude.val(position.coords.latitude);
                     longitude.val(position.coords.longitude);
                     accuracy.val(position.coords.accuracy);
+                    taken_at.val((new Date()).toISOString());
+                    bomb_form.submit();
                 },
                 locationError,
                 {
--- app/controllers/bombs_controller.rb
+++ app/controllers/bombs_controller.rb
@@ -1,5 +1,15 @@
 class BombsController < InheritedResources::Base
 
-  actions :new
+  actions :new, :create
+
+  def begin_of_association_chain
+    current_user
+  end
+
+  private
+
+    def bomb_params
+      params.require(:bomb).permit(:latitude, :longitude, :accuracy)
+    end
 
 end
