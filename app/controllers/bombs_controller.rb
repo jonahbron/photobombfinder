e9a5b0f554d5c46ed2fe7c91f33146df37d5b645
Jonah Dahlquist <jonah@nucleussystems.com>
2015-11-22 00:19:58 -0800

    Added succes message for adding photobomb

--- app/controllers/bombs_controller.rb
+++ app/controllers/bombs_controller.rb
@@ -2,6 +2,10 @@ class BombsController < InheritedResources::Base
 
   actions :new, :create
 
+  def create
+    create! :notice => 'Great, we\'ll let you know when you\'ve found your photobomb.'
+  end
+
   def begin_of_association_chain
     current_user
   end
