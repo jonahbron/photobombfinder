aa713b7e9c26f535e1efe887e6f4d225e786f6f3
Jonah Dahlquist <jonah@nucleussystems.com>
2015-11-22 14:21:12 -0800

    Changed order of results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @matches = current_user.matches
+    @matches = current_user.matches.order created_at: :desc
     @new_matches = @matches.where(seen_at: nil)
     @new_matches.each do |match|
       match.seen_at = Time.now
