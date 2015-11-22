c4b7bbc3f5cf056043ef3a28e6925059dfe600dc
Jonah Dahlquist <jonah@nucleussystems.com>
2015-11-22 13:02:20 -0800

    added debugging

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,7 @@ class PhotosController < InheritedResources::Base
     exifr = EXIFR::JPEG.new(photo_params['photo'].tempfile.path)
     @photo = Photo.new(photo_params)
     @photo.taken_at = exifr.date_time_digitized.to_datetime.utc
+    puts @photo.taken_at
     @photo.latitude = exifr.gps.latitude
     @photo.longitude = exifr.gps.longitude
     @photo.save!
